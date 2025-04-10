184d7e11ab72b7a9d634464836b23abb5ec67357
Seweryn Piórkowski <git@piorkowski.email>
2025-04-10 15:55:17 +0200

    add input and output paths args

--- converters/base.py
+++ converters/base.py
@@ -1,5 +1,8 @@
 import abc
+from pathlib import Path
 
 
 class BaseConverter(abc.ABC):
-    pass
+    def __init__(self, input_path: Path, output_path: Path) -> None:
+        self.input_path = input_path
+        self.output_path = output_path
