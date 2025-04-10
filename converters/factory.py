c9a0847bb8c25683f9571db1ec44ba1437651d62
Seweryn Piórkowski <git@piorkowski.email>
2025-04-10 16:37:58 +0200

    rename converter

--- converters/factory.py
+++ converters/factory.py
@@ -2,16 +2,16 @@ import enum
 from pathlib import Path
 
 from converters.base import BaseConverter
-from converters.txt import TXTConverter
+from converters.txt import UTF8Converter
 
 
 class ConverterType(enum.StrEnum):
-    TXT = enum.auto()
+    UTF8 = enum.auto()
 
 
 def create_converter(converter_type: ConverterType, input_path: Path, output_path: Path) -> BaseConverter:
     match converter_type:
-        case ConverterType.TXT:
-            return TXTConverter(input_path, output_path)
+        case ConverterType.UTF8:
+            return UTF8Converter(input_path, output_path)
         case _:
             raise NotImplementedError
--- converters/txt.py
+++ converters/txt.py
@@ -1,6 +1,6 @@
 from converters.base import BaseConverter
 
 
-class TXTConverter(BaseConverter):
+class UTF8Converter(BaseConverter):
     def _convert(self) -> None:
         print("converting file...")  # noqa: T201
