169a3c1445450d4db98635aeb73b15c0fe204023
Seweryn Piórkowski <git@piorkowski.email>
2025-04-10 16:24:45 +0200

    add args

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def run() -> None:
+from pathlib import Path
+from typing import Annotated
+
+import typer
+
+
+def main(input_path: Annotated[Path, typer.Option()], output_path: Annotated[Path, typer.Option()]) -> None:
     pass
 
 
 if __name__ == "__main__":
-    run()
+    typer.run(main)
