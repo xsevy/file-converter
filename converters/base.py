9db2f7d50bacb9d05270c1f328dfb564fbc85a37
Seweryn Piórkowski <git@piorkowski.email>
2025-04-10 16:07:00 +0200

    make validators argument default

--- converters/base.py
+++ converters/base.py
@@ -5,7 +5,12 @@ from typing import Callable
 
 
 class BaseConverter(abc.ABC):
-    def __init__(self, input_path: Path, output_path: Path, validators: Sequence[Callable[[Path, Path], None]]) -> None:
+    def __init__(
+        self,
+        input_path: Path,
+        output_path: Path,
+        validators: Sequence[Callable[[Path, Path], None]] = (),
+    ) -> None:
         self._input_path = input_path
         self._output_path = output_path
         self._validators = validators
